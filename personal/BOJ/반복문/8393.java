f2df3ad8668abdc7eaf2b706b76c99a65f172422
임보경 <qhrudlim@gmail.com>
2026-03-24 15:44:32 +0900

    add BOJ 8393 implementation

---
+++ personal/BOJ/반복문/8393.java
@@ -0,0 +1,20 @@
+// 합
+// bronze 5
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    br.close();
+
+    int sum = 0;
+    for (int i = 1; i <= n; i++) sum += i;
+
+    System.out.println(sum);
+  }
+}
